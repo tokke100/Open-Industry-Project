59dfbe44bbcac5478fbfcdad1d6728f35b9d2475
tokke <tokke100@gmail.com>
2023-04-02 13:39:53 +0200

    updated extret script
    Now it can move.

--- Assets/Scripts/RetExtConveyor.cs
+++ Assets/Scripts/RetExtConveyor.cs
@@ -1,6 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using libplctag;
+using libplctag.DataTypes;
+using libplctag.NativeImport;
+using System;
+using UnityEditor;
+using System.Threading.Tasks;
 
 public class RetExtConveyor : MonoBehaviour
 {
@@ -10,19 +14,79 @@ public class RetExtConveyor : MonoBehaviour
     public float ExtendSize;
     public float retractSize;
 
+    public bool enablePLC = false;
+    public string tagName;
+    public int speed;
+    int scantime = 0;
+    new readonly Tag<DintPlcMapper, int> tag = new();
+    int failCount = 0;
+
+    float moveTime = 0.0f;
+
+
+    private void Start()
+    {
+        if (enablePLC)
+        {
+            try { plctag.ForceExtractLibrary = false; } catch { };
+
+            var _plc = GameObject.Find("PLC").GetComponent<PLC>();
+
+            tag.Name = tagName;
+            tag.Gateway = _plc.Gateway;
+            tag.Path = _plc.Path;
+            tag.PlcType = _plc.PlcType;
+            tag.Protocol = _plc.Protocol;
+            tag.Timeout = TimeSpan.FromSeconds(5);
+
+            scantime = _plc.ScanTime;
+
+            InvokeRepeating(nameof(ScanTag), 0, (float)scantime / 1000f);
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
         if (Extend)
         {
-            Extend = false;
-            transform.localScale = new Vector3(3.5f, 1f, 1f);
+            moveTime += 1f * Time.deltaTime;
+            transform.localScale = new Vector3(Mathf.Lerp(retractSize, ExtendSize, moveTime), 1f, 1f);
+            if(moveTime >= 1)
+            {
+                transform.localScale = new Vector3(ExtendSize, 1f, 1f);
+                Extend = false;
+                moveTime = 0;
+            }
         }
 
         if (retract)
         {
-            retract = false;
-            transform.localScale = new Vector3(1f, 1f, 1f);
+            moveTime += 1f * Time.deltaTime;
+            transform.localScale = new Vector3(Mathf.Lerp(ExtendSize, retractSize, moveTime), 1f, 1f);
+            if (moveTime >= 1)
+            {
+                transform.localScale = new Vector3(retractSize, 1f, 1f);
+                retract = false;
+                moveTime = 0;
+            }
+        }
+    }
+
+    async Task ScanTag()
+    {
+        try
+        {
+            speed = await tag.ReadAsync();
+        }
+        catch (Exception)
+        {
+            if (failCount > 0)
+            {
+                CancelInvoke(nameof(ScanTag));
+                Debug.LogError($"Failed to read tag for object: {gameObject.name} check PLC object settings or Tag Name");
+            }
+            failCount++;
         }
     }
 }

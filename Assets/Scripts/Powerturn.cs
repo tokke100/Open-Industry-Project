ecac69266bb25e3fcdbe62ce8b5ec3df651895c1
ryevdokimov <robert.yevdokimov@autStand.com>
2023-04-04 10:55:25 -0500

    powerturn example

--- Assets/Scripts/Powerturn.cs
+++ Assets/Scripts/Powerturn.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class Powerturn : MonoBehaviour
 {
     Rigidbody rb;
+    Vector3 startAngle = new();
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        startAngle = transform.eulerAngles;
     }
 
     void FixedUpdate()
     {
         rb.angularVelocity = transform.TransformDirection(Vector3.up) * 1;
+        transform.eulerAngles= startAngle;
     }
 }
